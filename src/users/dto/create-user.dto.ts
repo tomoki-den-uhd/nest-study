35be898f0bec916f8083345f6f7e0c45550e01e4
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-09 17:16:31 +0900

    name email passwordのバリデーション追加

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,38 @@
+import { Options } from '@nestjs/common';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
   id: number;
+
+  @IsNotEmpty()
+  @MaxLength(50)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
 
 export class UpdateUserDto {
   id: number;
+
+  @IsNotEmpty()
+  @MaxLength(50)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
-  
+
+//- nameは必須で、最大長は50文字とします。
+// - emailは必須で、有効なメールアドレス形式でなければなりません。
+// - passwordは必須で、最小長は6文字とします。

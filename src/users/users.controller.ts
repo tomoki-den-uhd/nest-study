6ee569aae2468d84e63e3b5150f3b414a6c399ee
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-09 14:38:55 +0900

    httpメソッドの簡単な概要作成

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Users } from './users.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return 'This is findAll';
+  }
+
+  @Post()
+  create() {
+    return 'This is create';
+  }
+
+  @Put()
+  updateUser() {
+    return 'This is updateUser';
+  }
+
+  @Delete()
+  delete() {
+    return 'This is delete';
+  }
+}

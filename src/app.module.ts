fc38c57fa97309543f038123d0e6b36842168fce
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-09 13:55:44 +0900

    prismaインストール

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModuleModule } from './users-module/users-module.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModuleModule],
   controllers: [AppController],
   providers: [AppService],
 })

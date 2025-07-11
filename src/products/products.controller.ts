c6c0508ee4e0b179d41958c6d3aa1786af18c42c
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-11 09:48:39 +0900

    ProductController findAll作成

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from '../../generated/prisma';
+import { CreateProductDto, UpdateProductDto } from './dto/products.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(private readonly productsService: ProductsService) {}
 
   @Post('id')
   create() {
     return 'This is Product create';
   }
 
-  @Get('id')
-  findAll() {
-    return 'This is Product find All';
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return await this.productsService.findAll();
   }
 
   @Put('id')

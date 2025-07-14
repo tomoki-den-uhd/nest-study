524d8e30d81f7d1c23e6067192cb5de88d313caa
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-14 15:01:48 +0900

    authServiceにユーザ登録実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersService } from 'src/users/users.service';
-import { authUserDto } from './dto/auth-user.dto';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
 import { UpdateProductDto } from 'src/products/dto/products.dto';
 import { JwtService } from '@nestjs/jwt';
-import { CredentialsDto } from './dto/credentials.dto';
 import { JwtPayload } from 'src/types/jwtPayload';
 import * as bcrypt from 'bcrypt';
+import { EmailAlreadyExistsException } from 'src/users/users.exception';
+import { User } from 'generated/prisma';
 
 @Injectable()
 export class AuthService {
@@ -16,6 +16,24 @@ export class AuthService {
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
   ) {}
+
+  async signUp(credentialsDto: CredentialsDto): Promise<User> {
+    const emailExists = await this.prismaService.user.findUnique({
+      where: { email: credentialsDto.email },
+    });
+    if (emailExists) {
+      throw new EmailAlreadyExistsException('credentialsDto.email');
+    }
+    const { userName, email, password } = credentialsDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await this.prismaService.user.create({
+      data: {
+        userName,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
 
   async signIn(credentialsDto: CredentialsDto): Promise<{ token: string }> {
     const { email, password } = credentialsDto;
@@ -35,9 +53,4 @@ export class AuthService {
     }
     throw new UnauthorizedException();
   }
-
-  //   async createNewUser(authUserDto: authUserDto) {
-  //     const { name, userName, password } = authUserDto;
-  //     return await this.usersService.create(authUserDto);
-  //   }
 }

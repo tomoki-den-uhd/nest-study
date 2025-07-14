4ec4ae77555155b2fcf7b94f94a43ff9df2cd073
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-14 15:34:58 +0900

    JWT検証実装

---
+++ src/auth/dto/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/types/jwtPayload';
+import { RequestUser } from 'src/types/requestUser';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiretion: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payloard: JwtPayload): Promise<RequestUser> {
+    return {
+      id: payloard.sub,
+      userName: payloard.userName,
+    };
+  }
+}
---
+++ src/types/requestUser.ts
@@ -0,0 +1,4 @@
+export type RequestUser = {
+  id: number;
+  userName: string;
+};

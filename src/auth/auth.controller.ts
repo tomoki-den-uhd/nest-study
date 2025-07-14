2aecbd4d91a47eaf0f4e486a172b7f6ce22f9156
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-14 15:01:23 +0900

    authControllerにユーザ作成エンドポイント作成

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
+import { Body, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CredentialsDto } from './dto/credentials.dto';
+import { User } from 'generated/prisma';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async signUp(@Body() credentialsDto: CredentialsDto): Promise<User> {
+    return await this.authService.signUp(credentialsDto);
+  }
+
+  @Post('login')
+  async signIn(
+    @Body() credentials: CredentialsDto,
+  ): Promise<{ token: string }> {
+    return await this.authService.signIn(credentials);
+  }
+}

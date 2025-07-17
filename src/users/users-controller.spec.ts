632dc2e668e2fe384a301e299a0c2fd1f8646d30
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-17 14:59:40 +0900

    GET/users/:idメソッド追加

--- src/users/users-controller.spec.ts
+++ src/users/users-controller.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { NotFoundId } from './users.exception';
 
 describe('UsersCreateTest', () => {
   let usersController: UsersController;
@@ -9,6 +10,7 @@ describe('UsersCreateTest', () => {
 
   const mockUserService = {
     create: jest.fn(),
+    findById: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -40,6 +42,30 @@ describe('UsersCreateTest', () => {
       await expect(usersController.create(createUser)).resolves.toEqual(
         createUser,
       );
+    });
+  });
+
+  describe('GET/users/:id', () => {
+    it('存在しないIDを指定いる時', async () => {
+      (mockUserService.findById as jest.Mock).mockRejectedValue(
+        new NotFoundId(2),
+      );
+
+      await expect(mockUserService.findById(2)).rejects.toThrow(
+        `指定されたID:2は見つかりませんでした`,
+      );
+    });
+
+    it('正常値', async () => {
+      const getUser: CreateUserDto = {
+        id: 1,
+        userName: 'nageihgaiwe',
+        email: 'test@test.com',
+        password: 'password',
+      };
+
+      (mockUserService.findById as jest.Mock).mockResolvedValue(getUser);
+      await expect(usersController.findById(1)).resolves.toEqual(getUser);
     });
   });
 });

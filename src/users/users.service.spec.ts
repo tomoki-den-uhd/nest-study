6fc45fddadad582077849afbc0ac9f68801e8303
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-17 14:50:51 +0900

    UserService 修正
    coverageで検出されていなかった分岐のテストが通るように修正

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { EmailAlreadyExistsException, NotFoundId } from './users.exception';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 const mockPrismaService = {
   user: {
@@ -41,14 +42,15 @@ describe('UsersServieTest', () => {
         email: 'test@test.com',
         password: 'password',
       };
-      (prismaService.user.findUnique as jest.Mock).mockRejectedValue(
-        new EmailAlreadyExistsException('test@test.com'),
+
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(
+        createUserDto,
       );
+
       await expect(usersService.create(createUserDto)).rejects.toThrow(
         'メールアドレス:test@test.comは既に登録されています',
       );
     });
-
     it('正常系', async () => {
       const createUserDto: CreateUserDto = {
         id: 1,
@@ -69,9 +71,7 @@ describe('UsersServieTest', () => {
 
   describe('findById', () => {
     it('idが見つからなかった時', async () => {
-      (prismaService.user.findUnique as jest.Mock).mockRejectedValue(
-        new NotFoundId(2),
-      );
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(null);
 
       await expect(usersService.findById(2)).rejects.toThrow(
         `指定されたID:2は見つかりませんでした`,
@@ -93,13 +93,16 @@ describe('UsersServieTest', () => {
 
   describe('update', () => {
     it('idが見つからなかった時', async () => {
-      (prismaService.user.findUnique as jest.Mock).mockRejectedValue(
-        new NotFoundId(99868),
-      );
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(null);
 
-      await expect(usersService.findById(99868)).rejects.toThrow(
-        `指定されたID:99868は見つかりませんでした`,
-      );
+      await expect(
+        usersService.updateUser(99868, {
+          id: 1,
+          userName: 'test',
+          email: 'test1@test.com',
+          password: 'password',
+        }),
+      ).rejects.toThrow('指定されたID:99868は見つかりませんでした');
     });
 
     it('正常系', async () => {
@@ -110,27 +113,27 @@ describe('UsersServieTest', () => {
         password: 'hgkalhaiehga',
       };
 
-      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(297452);
-      (prismaService.user.update as jest.Mock).mockResolvedValue(updateUserDto);
-      await expect(
-        usersService.updateUser(297452, updateUserDto),
-      ).resolves.toEqual({
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue({
         id: 297452,
         userName: 'ganioseogah',
         email: 'ganjkeghwiyt@ngan.gaiue',
         password: 'hgkalhaiehga',
       });
+
+      (prismaService.user.update as jest.Mock).mockResolvedValue(updateUserDto);
+
+      await expect(
+        usersService.updateUser(297452, updateUserDto),
+      ).resolves.toEqual(updateUserDto);
     });
   });
 
   describe('delete', () => {
     it('idが見つからなかった時', async () => {
-      (prismaService.user.findUnique as jest.Mock).mockRejectedValue(
-        new NotFoundId(99868),
-      );
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(null);
 
-      await expect(usersService.findById(99868)).rejects.toThrow(
-        `指定されたID:99868は見つかりませんでした`,
+      await expect(usersService.delete(99868)).rejects.toThrow(
+        NotFoundException,
       );
     });
 
@@ -141,14 +144,12 @@ describe('UsersServieTest', () => {
         email: 'lajga@gnae.com',
         password: 'glairhaowe',
       };
-      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(93251);
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(
+        deleteUserDto,
+      );
       (prismaService.user.delete as jest.Mock).mockResolvedValue(deleteUserDto);
-      await expect(usersService.delete(93251)).resolves.toEqual({
-        id: 93251,
-        userName: 'anguaew',
-        email: 'lajga@gnae.com',
-        password: 'glairhaowe',
-      });
+
+      await expect(usersService.delete(93251)).resolves.toEqual(deleteUserDto);
     });
   });
 });

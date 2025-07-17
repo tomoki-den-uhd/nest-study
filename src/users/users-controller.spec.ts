8a37264040df9a3f3ea99e6aa84a498558810e28
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-17 14:58:03 +0900

    POST/usrers作成

--- src/users/users-controller.spec.ts
+++ src/users/users-controller.spec.ts
@@ -7,7 +7,9 @@ describe('UsersCreateTest', () => {
   let usersController: UsersController;
   let usersService: UsersService;
 
-  const mockUserService = {};
+  const mockUserService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -23,5 +25,21 @@ describe('UsersCreateTest', () => {
 
     usersController = module.get<UsersController>(UsersController);
     usersService = module.get<UsersService>(UsersService);
+  });
+
+  describe('POST/users', () => {
+    it('正常値', async () => {
+      const createUser: CreateUserDto = {
+        id: 1,
+        userName: 'nageihgaiwe',
+        email: 'test@test.com',
+        password: 'password',
+      };
+
+      (mockUserService.create as jest.Mock).mockResolvedValue(createUser);
+      await expect(usersController.create(createUser)).resolves.toEqual(
+        createUser,
+      );
+    });
   });
 });

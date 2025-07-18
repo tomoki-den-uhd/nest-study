9c60cd233cdbf9461ec388ab92685dae97c015f4
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-18 17:18:56 +0900

    バッチ処理の感覚を5分に変更

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -4,34 +4,32 @@ import * as cron from 'node-cron';
 
 @Injectable()
 export class BatchService {
-  //private readonly logger = new Logger(BatchService.name);
   constructor(private readonly prismaService: PrismaService) {
     this.scheduleStockRefill();
   }
 
+  async refillStock() {
+    const start = new Date();
+    try {
+      await this.prismaService.product.findFirstOrThrow({});
+      const refill = await this.prismaService.product.updateMany({
+        data: { stock: 10 },
+      });
+
+      console.log(`商品在庫補充バッチ開始時刻: ${start}`);
+      console.log(`処理実行件数:${Object.values(refill)}件`);
+
+      const end = new Date();
+      console.log(`商品在庫補充バッチ終了時刻: ${end}`);
+      return refill;
+    } catch (error) {
+      throw new Error('商品がDBに設定されていません');
+    }
+  }
+
   scheduleStockRefill() {
-    cron.schedule('*/10 * * * * *', async () => {
-      const start = new Date();
-
-      try {
-        await this.prismaService.product.findFirstOrThrow({});
-        const refill = await this.prismaService.product.updateMany({
-          data: { stock: 5 },
-        });
-
-        console.log(`商品在庫補充バッチ開始時刻: ${start}`);
-
-        console.log(`処理実行件数:${Object.values(refill)}件`);
-
-        const end = new Date();
-        console.log(`商品在庫補充バッチ終了時刻: ${end}`);
-      } catch (error) {
-        throw new Error('商品がDBに設定されていません');
-      }
-
-      //エラーハンドリング
-
-      //通知処理
+    cron.schedule('*/5 * * * *', async () => {
+      await this.refillStock();
     });
   }
 }

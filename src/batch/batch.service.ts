6a1a986d86d0fe4d6b1336227f154ec934fd0068
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-18 12:01:59 +0900

    在庫増やすバッチ処理
    ログ実装 エラーハンドリングまだ

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as cron from 'node-cron';
 
 @Injectable()
-export class BatchService {}
+export class BatchService {
+  private readonly logger = new Logger(BatchService.name);
+  constructor(private readonly prismaService: PrismaService) {
+    this.scheduleStockRefill();
+  }
+
+  scheduleStockRefill() {
+    cron.schedule('*/10 * * * * *', async () => {
+      const start = new Date();
+      const refill = await this.prismaService.product.updateMany({
+        data: { stock: 5 },
+      });
+
+      console.log(`商品在庫補充バッチ開始時刻: ${start}`);
+      console.log(`処理実行件数:${Object.values(refill)}件`);
+
+      const end = new Date();
+      console.log(`商品在庫補充バッチ終了時刻: ${end}`);
+
+      const noProduct = this.prismaService.product.findFirst();
+
+      //エラーハンドリング
+
+      //通知処理
+    });
+  }
+}

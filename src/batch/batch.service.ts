c9a75527b2e57fb864f883988c66a225b99c8496
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-18 13:51:41 +0900

    エラーハンドリング商品がデータベースにない時

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -4,7 +4,7 @@ import * as cron from 'node-cron';
 
 @Injectable()
 export class BatchService {
-  private readonly logger = new Logger(BatchService.name);
+  //private readonly logger = new Logger(BatchService.name);
   constructor(private readonly prismaService: PrismaService) {
     this.scheduleStockRefill();
   }
@@ -12,17 +12,22 @@ export class BatchService {
   scheduleStockRefill() {
     cron.schedule('*/10 * * * * *', async () => {
       const start = new Date();
-      const refill = await this.prismaService.product.updateMany({
-        data: { stock: 5 },
-      });
 
-      console.log(`商品在庫補充バッチ開始時刻: ${start}`);
-      console.log(`処理実行件数:${Object.values(refill)}件`);
+      try {
+        await this.prismaService.product.findFirstOrThrow({});
+        const refill = await this.prismaService.product.updateMany({
+          data: { stock: 5 },
+        });
 
-      const end = new Date();
-      console.log(`商品在庫補充バッチ終了時刻: ${end}`);
+        console.log(`商品在庫補充バッチ開始時刻: ${start}`);
 
-      const noProduct = this.prismaService.product.findFirst();
+        console.log(`処理実行件数:${Object.values(refill)}件`);
+
+        const end = new Date();
+        console.log(`商品在庫補充バッチ終了時刻: ${end}`);
+      } catch (error) {
+        throw new Error('商品がDBに設定されていません');
+      }
 
       //エラーハンドリング
 
